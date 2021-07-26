0a5421f461a972d7af9b2468fac026f63d564074
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-07-26 17:15:14 -0400

    now I took it off

--- src/CircleChartOfTransactions.js
+++ src/CircleChartOfTransactions.js
@@ -1,30 +1,9 @@
 import React from "react"
-import { CircleChart } from "playbook-ui"
-
-const dataWithLegend = [{
-  name: 'Bugs',
-  value: 8,
-
-},
-{
-  name: 'Chores',
-  value: 1,
-
-},
-{
-  name: 'Stories',
-  value: 12,
-},
-]
+// import { CircleChart } from "playbook-ui"
 
 const CircleChartOfTransactions = ({ props }) => (
   <div>
-    <CircleChart
-        chartData={dataWithLegend}
-        id="with-legend-example"
-        legend
-        {...props.category}
-    />
+   <h1> I WILL MAKE THIS WORK!</h1>
   </div>
 )
 
